4b264e5fa2f46308aded6c69ba01b82022e782ae
kunjshah4u <kunj08784@gmail.com>
2026-01-01 21:53:16 +0530

    remove unused css

--- electrolyzer-ui/src/components/ElectrolyzerSidebar.tsx
+++ electrolyzer-ui/src/components/ElectrolyzerSidebar.tsx
@@ -15,7 +15,7 @@ export const ElectrolyzerSidebar = ({
   selectedId,
   onSelect,
 }: SidebarProps) => (
-  <div className="w-[18rem] px-[2rem] shrink-0 flex flex-col bg-white">
+  <div className="w-[18rem] px-[2rem] shrink-0 flex flex-col">
     <span className="text-[1rem] mb-[1.5rem]">Available Electrolyzers ID</span>
 
     <div className="relative w-full mb-[2rem]">
@@ -25,20 +25,20 @@ export const ElectrolyzerSidebar = ({
       <input
         type="text"
         placeholder="Search Electrolyzer ID..."
-        className="w-auto pl-[2.2rem] pr-[0.5rem] py-[0.5rem] text-[0.8rem] rounded-[0.25rem] bg-white"
+        className="w-auto pl-[2.2rem] pr-[0.5rem] py-[0.5rem] text-[0.8rem] rounded-[0.25rem]"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
     </div>
 
-    <div className="flex flex-col gap-[0.75rem] overflow-y-auto">
+    <div className="flex flex-col gap-[0.75rem]">
       {filteredIds.length > 0 ? (
         filteredIds.map((id) => (
           <button
             key={id}
             onClick={() => onSelect(id)}
             className={`w-full max-w-[8rem] mb-[1rem] border-none py-[0.6rem] rounded-[0.5rem] text-[0.875rem] font-[500] ${
-              selectedId === id ? "bg-[#D97706] text-white" : "bg-[#D1D5DB]"
+              selectedId === id ? "bg-[#D97706]" : "bg-[#D1D5DB]"
             }`}
           >
             {id}
